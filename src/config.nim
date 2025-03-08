3fa00b0ce3012edea9a64e8a4b45d1cccdb9a6f5
Maxwell Jensen <85795372+maxwelljens@users.noreply.github.com>
2025-03-08 18:53:07 +0100

    Polished everything

---
+++ src/config.nim
@@ -0,0 +1,115 @@
+# Program written by Maxwell Jensen (c) 2025
+# Licensed under European Union Public Licence 1.2.
+# For more information, consult README.md or man page
+
+import streams, os, yaml, utils
+
+const DefaultConfig = readFile("default_config.yaml")
+
+type ShellCmd = object
+  engine* : string
+  args* : seq[string]
+
+type ConfigEngineEntry = object
+  name : string
+  path : string
+type ConfigIWadEntry = object
+  name : string
+  path : string
+type ConfigWadEntry = object
+  name* : string
+  path* : string
+type ConfigPresetEntry = object
+  name* : string
+  description* : string
+  engine* : string
+  iwad* : string
+  wad* : seq[string]
+  args* : seq[string]
+type Config = object
+  iwad : seq[ConfigIWadEntry]
+  engine : seq[ConfigEngineEntry]
+  wad* : seq[ConfigWadEntry]
+  preset* : seq[ConfigPresetEntry]
+
+proc loadEngine(shellCmd: ref ShellCmd, config: Config, specificPreset: ConfigPresetEntry) =
+  for x in config.engine:
+    if x.name == specificPreset.engine:
+      shellCmd.engine = x.path
+      return
+  throwError("No engine '" & specificPreset.engine & "' has been found.")
+
+proc loadIWad(shellCmd: ref ShellCmd, config: Config, specificPreset: ConfigPresetEntry) =
+  for x in config.iwad:
+    if x.name == specificPreset.iwad:
+      shellCmd.args.add(["-iwad", x.path])
+      return
+  throwError("No IWAD '" & specificPreset.iwad & "' has been found.")
+
+proc loadWads(shellCmd: ref ShellCmd, config: Config, specificPreset: ConfigPresetEntry) =
+  for x in specificPreset.wad:
+    var entryFound = false
+    for y in config.wad:
+      if x == y.name:
+        shellCmd.args.add(["-file", y.path])
+        entryFound = true
+    if not entryFound:
+      echoWarn("WAD '" & x & "' from preset '" & specificPreset.name & "' not found in WAD list.")
+    entryFound = false
+
+proc loadAdditionalArgs(shellCmd: ref ShellCmd, config: Config, specificPreset: ConfigPresetEntry) =
+  for x in specificPreset.args:
+    shellCmd.args.add(x)
+
+proc loadPreset(shellCmd: ref ShellCmd, config: Config, presetName: string) =
+  for x in config.preset:
+    if x.name == presetName:
+      shellCmd.loadEngine(config, x)
+      shellCmd.loadIWad(config, x)
+      shellCmd.loadWads(config, x)
+      shellCmd.loadAdditionalArgs(config, x)
+      return
+  throwError("No preset with given name '" & presetName & "' has been found.")
+
+proc createDefaultConfig() =
+  let configFilePath = getConfigDir() / "gibman" / "gibman.yaml"
+  if fileExists(configFilePath):
+    throwError("File already exists.")
+  try:
+    createDir(getConfigDir() / "gibman")
+    writeFile(configFilePath, DefaultConfig)
+    echo("No configuration file was found. Created configuration file at " &
+    configFilePath & ". It contains all available information to help you ",
+    "get started with your gibman use.")
+    quit(0)
+  except IOError as e:
+    throwError("Unable to place default configuration file at " &
+    configFilePath & ": " & e.msg & ". Permission error?")
+
+proc parseConfig*(preset: string): Config =
+  var config: Config
+  proc loadConfig(path: string) =
+    var s = newFileStream(path)
+    load(s, config)
+    s.close()
+  let configFilePathNoExt = getConfigDir() / "gibman" / "gibman"
+  let configFilePathYml = getConfigDir() / "gibman" / "gibman.yml"
+  let configFilePathYaml = getConfigDir() / "gibman" / "gibman.yaml"
+  if fileExists(configFilePathNoExt):
+    loadConfig(configFilePathNoExt)
+  elif fileExists(configFilePathYml):
+    loadConfig(configFilePathYml)
+  elif fileExists(configFilePathYaml):
+    loadConfig(configFilePathYaml)
+  else:
+    createDefaultConfig()
+  return config
+
+proc constructShellCmd*(config: Config, preset: string): ref ShellCmd =
+  var shellCmd = new(ShellCmd)
+  if preset != "":
+    shellCmd.loadPreset(config, preset)
+  else:
+    shellCmd.engine = config.engine[0].path
+    shellCmd.args.add(["-iwad", config.iwad[0].path])
+  return shellCmd
---
+++ src/utils.nim
@@ -0,0 +1,61 @@
+# Program written by Maxwell Jensen (c) 2025
+# Licensed under European Union Public Licence 1.2.
+# For more information, consult README.md or man page
+
+import std/envvars, rainbow, nancy, strutils
+
+proc throwError*(s: string, i: int = 1) =
+  if existsEnv("NO_COLOR"):
+    echo("ERROR: ", s)
+  else:
+    echo("ERROR: ".rfRed, s)
+  quit(i)
+
+proc echoWarn*(s: string) =
+  if existsEnv("NO_COLOR"):
+    echo("WARNING: ", s)
+  else:
+    echo("WARNING: ".rfYellow, s)
+
+proc echoPresetTable*[T](presets: seq[T]) =
+  var table: TerminalTable
+  var wads: string
+  if existsEnv("NO_COLOR"):
+    table.add "Name",
+      "Description",
+      "Engine",
+      "WADs"
+  else:
+    table.add "Name".rfYellow,
+      "Description".rfYellow,
+      "Engine".rfYellow,
+      "WADs".rfYellow
+  for x in presets:
+    if existsEnv("NO_COLOR"):
+      wads = wads & x.iwad & ", "
+    else:
+      wads = wads & x.iwad.rfGreen & ", "
+    for s in x.wad:
+      wads = wads & s & ", "
+    wads.removeSuffix(", ")
+    table.add x.name,
+      x.description,
+      x.engine,
+      wads
+    wads = ""
+  table.echoTableSeps(80, boxSeps)
+
+proc echoShellCmd*[T](shellCmd: T) =
+  var str: string
+  if existsEnv("NO_COLOR"):
+    str = str & shellCmd.engine & " "
+    for x in shellCmd.args:
+      str = str & x & " "
+  else:
+    str = str & shellCmd.engine.rfYellow & " "
+    for x in shellCmd.args:
+      if x.len > 0 and x[0] == '-':
+        str = str & x.rfGreen & " "
+      else:
+        str = str & x & " "
+  echo(str)
